87ae966eee403cc02ed04b92ca2f3df8623ab0a1
Sam Baldwin <sambaldwin92@gmail.com>
2022-06-06 12:43:51 +0100

    Restructuring and relearning webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
